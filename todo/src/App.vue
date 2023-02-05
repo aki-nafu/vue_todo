f3a3179d13b101e1a9618a12a95552ae40de6270
aki-nafu <akimifunakoshi@gmail.com>
2023-02-05 18:42:10 +0900

    todo追加&削除機能を実装

--- todo/src/App.vue
+++ todo/src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ToDoApp msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ToDoApp from './components/ToDoApp.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ToDoApp
   }
 }
 </script>
---
+++ todo/src/components/ToDoApp.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <h1>Todoリスト</h1>
+      <input v-model="todo"><br>
+    <button @click="addTodo">追加</button>
+    <ul>
+      <li v-for="(todo, index) in todos" :key="index">{{ todo }} <span @click="removeTodo(index)">X</span></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      todo: '',
+      todos:[]
+    }
+  },
+  methods: {
+    addTodo() {
+      this.todos.push(this.todo)
+      this.todo = ''
+    },
+    removeTodo(index) {
+      this.todos.splice(index, 1)
+    }
+  }
+}
+</script>
+
+<style>
+</style>
