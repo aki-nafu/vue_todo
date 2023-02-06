884fc418462e8822cbc78c94bbd66c998cc418e9
aki-nafu <akimifunakoshi@gmail.com>
2023-02-06 21:57:52 +0900

    todoをオブジェクトに変更

--- todo/src/components/ToDoApp.vue
+++ todo/src/components/ToDoApp.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="app">
     <h1>Todoリスト</h1>
-      <input v-model="todo"><br>
+      <input v-model="todo.name"><br>
     <button @click="addTodo">追加</button>
     <ul>
-      <li v-for="(todo, index) in todos" :key="index">
-        <input type="checkbox" @click="removeTodo(index)" >{{ todo }} 
+      <li v-for="(todo, index) in todos" :key="todo">
+        <input type="checkbox" @click="removeTodo(index)" >{{ todo.name }} 
       </li>
     </ul>
   </div>
@@ -15,7 +15,10 @@
 export default {
   data() {
     return {
-      todo: '',
+      todo: {
+        'name': name,
+        'done': true
+      },
       todos:[]
     }
   },
