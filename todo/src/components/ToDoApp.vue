ff8247cfc0828e3372c8ae9b018d4f7c17c240bb
aki-nafu <akimifunakoshi@gmail.com>
2023-02-07 20:46:39 +0900

    todoのdataの形式を変更

--- todo/src/components/ToDoApp.vue
+++ todo/src/components/ToDoApp.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="app">
     <h1>Todoリスト</h1>
-      <input v-model="todo.name"><br>
+      <input v-model="todoName"><br>
     <button @click="addTodo">追加</button>
     <ul>
       <li v-for="(todo, index) in todos" :key="todo">
-        <input type="checkbox" @click="removeTodo(index)" >{{ todo.name }} 
+        <input type="checkbox" v-model="todo.isCompleted" @click="removeTodo(index)" >{{ todo.name }} 
       </li>
     </ul>
   </div>
@@ -15,17 +15,15 @@
 export default {
   data() {
     return {
-      todo: {
-        'name': name,
-        'done': true
-      },
+      todoName:'',
       todos:[]
     }
   },
   methods: {
     addTodo() {
-      this.todos.push(this.todo)
-      this.todo = ''
+      if (this.todoName == '') return;
+      this.todos.push({name: this.todoName, isCompleted: false})
+      this.todoName = ''
     },
     removeTodo(index) {
       this.todos.splice(index, 1)
